9a724c4b0c2db47177f79838b630607b12ecf122
aditya <aditya.1923it1006@kiet.edu>
2022-02-22 23:34:13 +0530

    commit

---
+++ Basics_of_Javascript/Day12/Data_types.js
@@ -0,0 +1,14 @@
+//javascript is dynamically tight which means we do not have to define the type of the variable
+function test(){
+    var a = 1    //Number
+    var b = 1.020 //Floating Number
+    var c = "ABC"  //String
+    var d = 'A'   //Character
+    var e = `I am a multi-line string and closed in back ticks` //Multiline string
+    var f = [1,2,3,4,5] //Array
+    var g = ["aa","bb","cc"]  //String as an array
+    var h = [{name: "Aditya" , age:21}]  //Objects
+    //In javascript most of the thing is object.
+    //OBJECT: key:value pair is an object
+
+}
